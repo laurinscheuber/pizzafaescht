8c23055290d723b062ad2ab6d6f602b0a323b81b
laurinscheuber <38795964+laurinscheuber@users.noreply.github.com>
2024-02-13 08:38:56 +0100

    adding tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors:{
+        'pifae-orange': '#FF9A00',
+        'pifae-darkblue': '#223442',
+        'pifae-lightblue': '#3A506B',
+
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
